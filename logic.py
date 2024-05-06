ec531982a9cea2585fed8ef0f39b1c22d570609d
Kootyra <110038102+Kootyra@users.noreply.github.com>
2024-05-06 17:27:11 +0200

    first commit

--- logic.py
+++ logic.py
@@ -1,38 +1,42 @@
-from random import randint
-import requests
+import aiohttp  # Библиотека для асинхронных HTTP запросов
+import random
 
 class Pokemon:
     pokemons = {}
     # Инициализация объекта (конструктор)
     def __init__(self, pokemon_trainer):
-
-        self.pokemon_trainer = pokemon_trainer   
-
-        self.pokemon_number = randint(1,1000)
-        self.img = self.get_img()
-        self.name = self.get_name()
-
-        Pokemon.pokemons[pokemon_trainer] = self
-
-    # Метод для получения картинки покемона через API
-    def get_img(self):
-        pass
-    
-    # Метод для получения имени покемона через API
-    def get_name(self):
-        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'
-        response = requests.get(url)
-        if response.status_code == 200:
-            data = response.json()
-            return (data['forms'][0]['name'])
+        self.pokemon_trainer = pokemon_trainer
+        self.pokemon_number = random.randint(1, 1000)
+        self.name = None
+        if pokemon_trainer not in Pokemon.pokemons:
+            Pokemon.pokemons[pokemon_trainer] = self
         else:
-            return "Pikachu"
+            self = Pokemon.pokemons[pokemon_trainer]
 
+    async def get_name(self):
+        # Асинхронный метод для получения имени покемона через PokeAPI
+        url = f'https://pokeapi.co/api/v2/pokemon/{self.pokemon_number}'  # URL API для запроса
+        async with aiohttp.ClientSession() as session:  # Открытие сессии HTTP
+            async with session.get(url) as response:  # Отправка GET запроса
+                if response.status == 200:
+                    data = await response.json()  # Получение и декодирование JSON ответа
+                    return data['forms'][0]['name']  # Возврат имени покемона
+                else:
+                    return "Pikachu"  # Возврат стандартного имени, если запрос не удался
 
-    # Метод класса для получения информации
-    def info(self):
-        return f"Имя твоего покеомона: {self.name}"
+    async def info(self):
+        # Метод возвращает информацию о покемоне
+        if not self.name:
+            self.name = await self.get_name()  # Получение имени, если оно еще не загружено
+        return f"Имя твоего покемона: {self.name}"  # Возвращение строки с именем покемона
 
-    # Метод класса для получения картинки покемона
-    def show_img(self):
-        return self.img
+    async def show_img(self):
+        # Асинхронный метод для получения изображения покемона
+        url = f"https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/{self.pokemon_number}.png"  # URL изображения покемона
+        async with aiohttp.ClientSession() as session:  # Открытие сессии HTTP
+            async with session.get(url) as resp:  # Отправка GET запроса на получение изображения
+                if resp.status == 200:
+                    data = await resp.read()  # Чтение байтов изображения
+                    return data  # Возврат байтов изображения
+                else:
+                    return None  # Возврат None, если запрос не удался
--- main.py
+++ main.py
@@ -1,18 +1,41 @@
-import telebot 
+import discord
+from discord.ext import commands
 from config import token
-
 from logic import Pokemon
+from discord import File
+from io import BytesIO
 
-bot = telebot.TeleBot(token) 
+# Настройка интентов для бота
+intents = discord.Intents.default()  # Получаем настройки по умолчанию
+intents.messages = True              # Разрешаем боту обрабатывать сообщения
+intents.message_content = True       # Разрешаем боту читать содержимое сообщений
+intents.guilds = True                # Разрешаем боту работать с серверами (guilds)
 
-@bot.message_handler(commands=['go'])
-def go(message):
-    if message.from_user.username not in Pokemon.pokemons.keys():
-        pokemon = Pokemon(message.from_user.username)
-        bot.send_message(message.chat.id, pokemon.info())
-        bot.send_photo(message.chat.id, pokemon.show_img())
+# Создание бота с заданным префиксом команд и активированными интентами
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+# Событие, которое срабатывает, когда бот готов к работе
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name}')  # Выводит в консоль имя бота
+
+# Команда '!go'
+@bot.command()
+async def go(ctx):
+    author = ctx.author.name  # Получаем имя автора сообщения
+    # Проверяем, есть ли уже покемон для этого пользователя
+    if author not in Pokemon.pokemons.keys():
+        pokemon = Pokemon(author)  # Создаем нового покемона
+        await ctx.send(await pokemon.info())  # Отправляем информацию о покемоне
+        image_data = await pokemon.show_img()  # Получаем изображение покемона
+        if image_data:
+            image_stream = BytesIO(image_data)  # Создаем поток для изображения
+            image_stream.seek(0)  # Перемещаем указатель в начало потока
+            await ctx.send(file=File(fp=image_stream, filename='pokemon.png'))  # Отправляем изображение
+        else:
+            await ctx.send("Не удалось загрузить изображение покемона.")
     else:
-        bot.reply_to(message, "Ты уже создал себе покемона")
+        await ctx.send("Ты уже создал себе покемона.")  # Сообщение, если покемон уже создан
 
-
-bot.infinity_polling(none_stop=True)
+# Запуск бота
+bot.run(token)
